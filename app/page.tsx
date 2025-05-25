8248793b554f53caa604b0ffd61fadadd709961d
Razvan Tomegea <razvan.tomegea@gmail.com>
2025-05-25 12:08:12 +0300

    Fixed padding

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,7 @@ export default function LandingPage() {
         )}
       </header>
       <main className="flex-1 overflow-x-hidden">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_500px]">
               <div className="flex flex-col justify-center space-y-4">
